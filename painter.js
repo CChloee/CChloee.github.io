22393fc320fa8ddbc4ba6cb70655b5f08dd6f360
Chloe Lim <bigchloe96@gmail.com>
2019-04-10 21:09:25 +0800

    finger drawing test2

--- painter.js
+++ painter.js
@@ -34,62 +34,100 @@ document.addEventListener('mousemove', function (event) {
 
 //---------------Finger drawing on canvas (touchEvent)------------------
 // function
-var mousePos = {x:0, y:0};
-var lastPos = mousePos;
-
-canvas.addEventListener('touchstart', function(e){
-      mousePos = getTouchPos(canvas, e);
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent('mousedown', {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener('touchend', function(e){
-  var mouseEvent = new MouseEvent('mouseup',{});
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener('touchmove', function(e){
-  var touch = e.touchs[0];
-  var mouseEvent = new MouseEvent('mousemove',{
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-//Get the position of a touch relative to the Canvas
-function getTouchPos(canvasDom, touchEvent){
-  var rect = canvasDOM.getBoundingClientRect();
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top
-  };
-}
-
-//Prevent scrolling when touching the canvas
-document.body.addEventListener('touchstart', function (e){
-  if (e.target == canvas){
-    e.preventDefault();
-  }
-}, false);
-
-document.body.addEventListener('touchend', function (e){
-  if (e.target == canvas){
-    e.preventDefault();
-  }
-}, false);
-
-document.body.addEventListener('touchmove', function(e){
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
+// var mousePos = {x:0, y:0};
+// var lastPos = mousePos;
+//
+// canvas.addEventListener('touchstart', function(e){
+//       mousePos = getTouchPos(canvas, e);
+//   var touch = e.touches[0];
+//   var mouseEvent = new MouseEvent('mousedown', {
+//     clientX: touch.clientX,
+//     clientY: touch.clientY
+//   });
+//   canvas.dispatchEvent(mouseEvent);
+// }, false);
+//
+// canvas.addEventListener('touchend', function(e){
+//   var mouseEvent = new MouseEvent('mouseup',{});
+//   canvas.dispatchEvent(mouseEvent);
+// }, false);
+//
+// canvas.addEventListener('touchmove', function(e){
+//   var touch = e.touchs[0];
+//   var mouseEvent = new MouseEvent('mousemove',{
+//     clientX: touch.clientX,
+//     clientY: touch.clientY
+//   });
+//   canvas.dispatchEvent(mouseEvent);
+// }, false);
+//
+// //Get the position of a touch relative to the Canvas
+// function getTouchPos(canvasDom, touchEvent){
+//   var rect = canvasDOM.getBoundingClientRect();
+//   return {
+//     x: touchEvent.touches[0].clientX - rect.left,
+//     y: touchEvent.touches[0].clientY - rect.top
+//   };
+// }
+//
+// //Prevent scrolling when touching the canvas
+// document.body.addEventListener('touchstart', function (e){
+//   if (e.target == canvas){
+//     e.preventDefault();
+//   }
+// }, false);
+//
+// document.body.addEventListener('touchend', function (e){
+//   if (e.target == canvas){
+//     e.preventDefault();
+//   }
+// }, false);
+//
+// document.body.addEventListener('touchmove', function(e){
+//   if (e.target == canvas) {
+//     e.preventDefault();
+//   }
+// }, false);
 
 //--------------------------------------------------------------------
+
+
+
+//----------------------------test 02---------------------------
+var touchX, touchY;
+
+function sketchpad_touchStart(){
+  getTouchPos();
+  drawDot(context, touchX, touchY, 12);
+
+  event.preventDefault();
+}
+
+function sketch_touchMove(e){
+  getTouchPos(e);
+  drawDot(context, touchX, touchY, 12);
+  event.preventDefault();
+}
+
+function getTouchPos(e){
+  if(!e)
+    var e = event;
+
+  if(e.touches){
+    if(e.touches.length == 1){
+      var touch = e.touches[0];
+      touchX=touch.pageX-touch.target.offsetLeft;
+      touchY= touch.pageY-touch.target.offsetTop;
+    }
+  }
+}
+
+canvas.addEventListener('touchstart', sketchpad_touchStart, false);
+canvas.addEventListener('touchmove', sketch_touchMove, false);
+
+//---------------Finger drawing on canvas (touchEvent)------------------
+//----------------------------------------------------------------------
+
 
 
 //Color swatches
