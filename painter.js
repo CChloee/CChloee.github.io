5672b19ccfcda5e616053bb045d5df2f8646428d
Chloe Lim <bigchloe96@gmail.com>
2019-04-10 20:38:56 +0800

    readjust bookmap element position & add touchEvent

--- painter.js
+++ painter.js
@@ -1,4 +1,5 @@
-const canvas = document.querySelector("canvas");
+//set up the canvas
+const canvas = document.getElementById("canvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -6,30 +7,89 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
 
 
-//Drawing on Painter
+//Drawing event on canvas (MouseEvent)
 context.strokeStyle = '#ff4141';
 context.lineWidth = 5;
 context.lineJoin = "round";
 context.lineCap = "round";
 
-let shouldPaint = false;
+let drawing = false;
 
 document.addEventListener('mousedown', function (event){
-  shouldPaint = true;
+  drawing = true;
   context.moveTo(event.pageX, event.pageY);
   context.beginPath();
 })
 
 document.addEventListener('mouseup', function (event){
-  shouldPaint = false;
+  drawing = false;
 })
 
 document.addEventListener('mousemove', function (event) {
-  if (shouldPaint){
+  if (drawing){
     context.lineTo(event.pageX, event.pageY);
     context.stroke();
   }
 })
+
+//---------------Finger drawing on canvas (touchEvent)------------------
+// function
+var mousePos = {x:0, y:0};
+var lastPos = mousePos;
+
+canvas.addEventListener('touchstart', function(e){
+      mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent('mousedown', {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener('touchend', function(e){
+  var mouseEvent = new MouseEvent('mouseup',{});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener('touchmove', function(e){
+  var touch = e.touchs[0];
+  var mouseEvent = new MouseEvent('mousemove',{
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+//Get the position of a touch relative to the Canvas
+function getTouchPos(canvasDom, touchEvent){
+  var rect = canvasDOM.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+//Prevent scrolling when touching the canvas
+document.body.addEventListener('touchstart', function (e){
+  if (e.target == canvas){
+    e.preventDefault();
+  }
+}, false);
+
+document.body.addEventListener('touchend', function (e){
+  if (e.target == canvas){
+    e.preventDefault();
+  }
+}, false);
+
+document.body.addEventListener('touchmove', function(e){
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+//--------------------------------------------------------------------
 
 
 //Color swatches
